ac7523676ba241637600bbc374d1d23730c75cb0
Simone Messaggi <simone.messaggi@generali.com>
2023-03-07 10:41:33 +0100

    Now fully working on postgresql! / install fix

--- install_directus.sh
+++ install_directus.sh
@@ -9,9 +9,9 @@ if [ ! $? -eq 0 ]
         echo "exiting..."
     else
     kubectl apply -f storage
-    echo;echo Installing pre-requisites...
-    # sudo mkdir -p /var/dockerdata/directus/db /var/dockerdata/directus/redis-master /var/dockerdata/directus/redis-replica /var/dockerdata/directus/postgresql
-    # sudo chmod a+w /var/dockerdata/directus/ -R
+    echo;echo Installing pre-requisites, this will create shared directories on the assumption your current host is also your k8s dev environment...
+    sudo mkdir -p /var/dockerdata/directus/db /var/dockerdata/directus/redis-master /var/dockerdata/directus/redis-replica /var/dockerdata/directus/postgresql
+    sudo chmod a+w /var/dockerdata/directus/ -R
     helm repo add bitnami https://charts.bitnami.com/bitnami
     cd charts/directus
     helm dependency update
@@ -20,5 +20,6 @@ if [ ! $? -eq 0 ]
     helm install -f directus-values.yaml -n directus directus ./charts/directus
     echo;echo "Here's your credentials: admin@example.com / d1r3ctu5"
     echo;echo "You should point the domain directus.local to your k8s node ip!"
+    echo;echo "Find out more about your WSL instance ip on powershell via: `wsl hostname -I`"
 fi
 echo "done."
